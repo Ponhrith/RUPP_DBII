ba12aeade4e4c46cab97a6f7f2083bbc52f2ebff
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 23:00:32 +0700

    Slide 23

---
+++ Intro_to_Python/slide23.py
@@ -0,0 +1,13 @@
+fruits = ["apple","banana","cherry","apple"]
+print(fruits.count("apple"))
+
+fruits = ["apple","banana","cherry","apple"]
+print(fruits.index("banana"))
+
+fruits_tuple = ("apple","banana","cherry")
+fruits_list = list(fruits_tuple)
+print(fruits_list)
+
+fruits_list = ["apple","banana","cherry"]
+fruits_tuple = tuple(fruits_list)
+print(fruits_tuple)
