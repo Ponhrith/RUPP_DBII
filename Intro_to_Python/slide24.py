e09206a893d11b0d1f8fbe3ee5ee9d088897d9e1
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 23:00:58 +0700

    Slide 24

---
+++ Intro_to_Python/slide24.py
@@ -0,0 +1,19 @@
+person = {"name": "Alice", "age":25 , "city": "New York"}
+print(person["name"])
+print(person["age"])
+
+person = {"name": "Alice", "age":25 , "city": "New York"}
+person["age"] = 26
+print(person)
+
+person = {"name": "Alice", "age":25 , "city": "New York"}
+person["gender"] = "Female"
+print(person)
+
+person = {"name": "Alice", "age":25 , "city": "New York"}
+del person["age"]
+print(person)
+
+age = person.pop("age")
+print(age)
+print(person)
