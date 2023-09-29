a927ee1a815922eccbe274ddc5ae9034548d6055
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 22:47:05 +0700

    Slide 11

---
+++ Intro_to_Python/slide11.py
@@ -0,0 +1,11 @@
+print('Hello World')
+
+print ('Hello','World')
+
+name = "Alice"
+print("My Name is", name)
+
+age = 25
+print (f"I am (agse) years old.")
+
+print ("one", "two", "three", sep="-")
