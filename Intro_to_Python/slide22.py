dc8490540d7bc4fe5495dfd3e7d7f715ec1ed97e
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 23:00:06 +0700

    Slide 22

---
+++ Intro_to_Python/slide22.py
@@ -0,0 +1,18 @@
+fruits =("apple","banana","cherry")
+print(fruits[0])
+print(fruits[1])
+
+fruits = ("apple","banana","cherry")
+for fruit in fruits:
+    print(fruits)
+
+fruits = "apple", "banana"
+a, b = fruits
+print(a)
+print(b)
+
+a = 10
+b = 20
+a, b = b, a
+print(a)
+print(b)
