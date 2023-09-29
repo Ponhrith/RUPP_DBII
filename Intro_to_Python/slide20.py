968ee7fc0069d51d705efb617251dea90fea3ea2
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 22:59:16 +0700

    Slide 20

---
+++ Intro_to_Python/slide20.py
@@ -0,0 +1,15 @@
+fruits = ["apple","banana","cherry"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+
+fruits = ["apple","banana","cherry","date","elderberry"]
+
+print(fruits[1:4])
+print(fruits[:3])
+print(fruits)
+
+fruits = ["apple","banana","cherry"]
+fruits[0] = "orange"
+print(fruits)
