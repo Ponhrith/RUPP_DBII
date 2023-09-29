6bd6df80054059dbc882149121b3a62fa783e94c
Ponhrith <ponhrith.nhep@gmail.com>
2023-09-29 23:01:48 +0700

    Slide 28

---
+++ Intro_to_Python/slide28.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,4,5]
+squared_numbers = [x**2 for x in numbers]
+print(squared_numbers)
+
+numbers = [1,2,3,4,5]
+squared_even_numbers = [x**2 for x in numbers if x % 2 == 0]
+print(squared_even_numbers)
