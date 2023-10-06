af0dad691a6403c5fd24d84b8df3f6eebf57b5ff
Ponhrith <ponhrith.nhep@gmail.com>
2023-10-06 23:02:42 +0700

    Slide11.py

--- Lab2/Slide12.py
+++ Lab2/Slide12.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from markupsafe import escape
 app = Flask(__name__)
+
 @app.route('/user/<username>')
 def show_user_profile(username):
           return f'User {escape(username)}'
